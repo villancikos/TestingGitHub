50ddd66a44a8acc91ed3e3f88b62b21381c2fd1e
villancikos <villancikos@gmail.com>
2017-01-19 13:10:58 +0000

    Archivo python

---
+++ test.py
@@ -0,0 +1,9 @@
+def hello_world(message):
+    print("Imprimiendo mi mensaje: {0}".format(message))
+    print("###################3")
+    print("###################3")
+    print("###################3")
+    print("###################3")
+
+ 
+hello_world("Mi mensaje es este!!!")
